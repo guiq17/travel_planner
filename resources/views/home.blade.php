47ee5ec786efe6ff37d422485839bf1eaa5a8703
guiqi97@gmail.com <guiqi97@gmail.com>
2023-11-24 17:31:54 +0900

    ホーム画面レイアウト完成(文章は仮です)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight text-center">
+            いま、旅に出よう
+        </h2>
+    </x-slot>
+    <div class="relative min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat" style="background-image: url('/images/bg_travel2.jpeg');">
+        <div class="absolute inset-0 flex items-center justify-center">
+            <div class="text-white text-center">
+                <h2 class="text-3xl font-semibold">旅のしおりを作成しよう</h2>
+                <h3 class="text-3xl font-semibold">旅行のしおり、予定表、計画を誰でも簡単に作れる無料のサービス「torapura」<br>しおりをメンバーと共有できます</h3>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
