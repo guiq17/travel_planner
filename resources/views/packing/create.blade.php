fd54b947c8f499e665ad671fefa73245708f95b4
guiqi97@gmail.com <guiqi97@gmail.com>
2023-11-29 10:24:21 +0900

    持ち物リスト関連のbladeファイル作成

---
+++ resources/views/packing/create.blade.php
@@ -0,0 +1,39 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            持ち物リスト作成
+        </h2>
+    </x-slot>
+    @include('components.complete_message')
+    @include('components.validate_message')
+    <div class="flex justify-center items-center min-h-screen" style="background-image: url('/images/main_bg6.jpeg'); background-size: cover;">
+        <div class="container mx-auto p-8 bg-white rounded-lg shadow-md max-w-5xl">
+            {{-- 持ち物登録フォーム --}}
+            <form action="{{ route('travel.store') }}" method="POST" class="bg-white p-6 rounded-lg shadow-md bg-gradient-to-b from-indigo-400 to-cyan-400">
+                @csrf
+                <div class="grid grid-cols-2 gap-6">
+                    {{-- タイトル入力 --}}
+                    <div class="col-span-2 mt-8">
+                        <label for="title" class="block text-lg font-medium text-gray-700 pd-4">タイトル</label>
+                        <input type="text" name="title" value="{{ old('title') }}" class="w-full p-2 rounded-md border border-gray-300" placeholder="タイトル入力">
+                    </div>
+                    {{-- 開始日入力 --}}
+                    <div>
+                        <label for="start_date" class="block text-lg font-medium text-gray-700">出発日</label>
+                        <input type="date" name="start_date" value="{{ old('start_date') }}" min="{{ now()->toDateString() }}" class="w-full p-2 rounded-md border border-gray-300" placeholder="出発日入力">
+                    </div>
+                    {{-- 終了日入力 --}}
+                    <div>
+                        <label for="end_date" class="block text-lg font-medium text-gray-700">帰宅日</label>
+                        <input type="date" name="end_date" value="{{ old('end_date') }}" min="{{ now()->toDateString() }}" class="w-full p-2 rounded-md border border-gray-300" placeholder="帰宅日入力">
+                    </div>
+                </div>
+                {{-- 保存ボタン --}}
+                <div class="mt-20">
+                    <button type="submit" class="inline-block px-4 py-2 mr-2 bg-white text-red-500 border-2 border-red-500 rounded-md hover:bg-red-500 hover:text-white"><a href="{{ route('travel.list') }}">キャンセル</a></button>
+                    <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">保存</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</x-app-layout>
