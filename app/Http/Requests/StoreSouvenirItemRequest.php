6f28d6da9e6133f1713df01ed3a79e2c92c92653
Kazumean <kazumin.angelav@gmail.com>
2023-12-07 15:44:22 +0900

    お土産追加時に、同一カテゴリー内には同一品名を登録できないように修正。

--- app/Http/Requests/StoreSouvenirItemRequest.php
+++ app/Http/Requests/StoreSouvenirItemRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
 
 class StoreSouvenirItemRequest extends FormRequest
 {
@@ -21,9 +23,22 @@ class StoreSouvenirItemRequest extends FormRequest
      */
     public function rules(): array
     {
+        $travel_id = $this->route('travel_id');
         return [
             'category_id' => 'required|numeric',
-            'souvenir_name' => 'required|string',
+            'souvenir_name' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('souvenir_items', 'name')
+                ->where(function ($query) use ($travel_id) {
+                    $query->join('souvenir_category_item', function ($join) use ($travel_id) {
+                            $join->on('souvenir_category_item.souvenir_item_id', '=', 'souvenir_items.id')
+                                 ->where('souvenir_category_item.travel_id', $travel_id);
+                        })
+                        ->where('souvenir_items.name', $this->souvenir_name);
+                })
+            ],
             'quantity' => 'required|numeric',
             'price' => 'nullable|numeric',
             'url' => 'nullable|url',
